4503f1f737d52d38a35ed3c318782b2ecca95a20
reinexxism <lucy0805@naver.com>
2022-09-17 19:45:37 +0900

    제어 문자를 사용한 출력

--- 2-3.c
+++ 2-3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-	// \n : 줄바꿈 실행
+	// \n(new line) : 다음 줄로 이동
 	printf("Be happy\n");
 	printf("12345678901234567890\n");
 	printf("My\tfriend\n");
-	// \t : 탭 위치로 이동
-	// \b : 앞뒤에 있는 문자를 서로 변경
+	// \t(tab) : 탭 위치로 이동
+	// \b(backspace) : 한 칸 왼쪽으로 이동
 	printf("Goot\bd\tchance\n");
-	// \r : 뒤의 문자를 맨 앞으로 이동
-	// \a : 경고음(alert)을 냄
+	// \r(carriage return) : 맨 앞으로 이동
+	// \a(alert) : 벨소리 출력
 	printf("Cow\rW\a\n");
 
 	return 0;
