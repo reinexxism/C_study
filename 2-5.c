8543d2ab058950d20f51df4ee88f55b1c997bf61
joohyun <70502744+reinexxism@users.noreply.github.com>
2022-09-18 13:26:12 +0900

    세 가지 진법의 정수 상수

--- 2-5.c
+++ 2-5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-	printf("%d\n", 12); // 10진수 표현법
-	printf("%d\n", 014); // 8진수 표현법
-	printf("%d\n", 0xc); // 16진수 표현법
+	printf("%d\n", 13); // 10진수 표현법
+	printf("%d\n", 015); // 8진수 표현법
+	printf("%d\n", 0xd); // 16진수 표현법
 
 	return 0;
 }
