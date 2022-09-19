9602d9f859b1a91df10c49fed6d5361c75288810
reinexxism <lucy0805@naver.com>
2022-09-19 15:37:33 +0900

    문자와 문자열 데이터의 출력

---
+++ 2-7.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("%c\n", 'A'); // 문자 상수 출력 : 작은 따옴표(%c)
+	printf("%s\n", "A"); // 문자열 상수 출력 : 큰 따옴표(%s)
+	printf("%c은 %s입니다.\n", '1', "First");
+
+	return 0;
+}
