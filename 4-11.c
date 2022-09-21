679de29806fcdfee5e784d9b4a63276e1069af4b
reinexxism <lucy0805@naver.com>
2022-09-21 20:10:37 +0900

    콤마 연산자

--- 4-11.c
+++ 4-11.c
@@ -4,7 +4,7 @@ int main() {
 	int a = 10, b = 20;
 	int res;
 
-	res = (++a, ++b);
+	res = (++a, ++b); // 차례로 연산이 수행되며, 결과적으로 res에 저장되는 값은 b의 값
 
 	printf("a : %d, b : %d\n", a, b);
 	printf("res : %d\n", res);
